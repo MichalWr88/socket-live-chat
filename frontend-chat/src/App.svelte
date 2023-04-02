7f32db2825fa8fbcaea1a3b4e68cdf55eccc64e5
mmaleszewski <mmaleszewski@trans.eu>
2023-04-02 19:27:33 +0200

    chore: add first room list

--- frontend-chat/src/App.svelte
+++ frontend-chat/src/App.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-  import WorkspaceList from './components/WorkspaceList.svelte'
-  import TailwindCss from './TailwindCSS.svelte';
+  import RoomsList from "./components/RoomsList.svelte";
+  import TextAreaMsg from "./components/TextAreaMsg.svelte";
+  import WorkspaceList from "./components/WorkspaceList.svelte";
+  import TailwindCss from "./TailwindCSS.svelte";
 </script>
 
-<main class="w-full container border-2 border-solid border-gray-700 h-full overflow-hidden">
+<main
+  class="w-full container border-2 border-solid border-gray-700 h-full overflow-hidden flex"
+>
   <TailwindCss />
-<WorkspaceList/>
+  <WorkspaceList />
+  <RoomsList />
+  <TextAreaMsg />
 </main>
 
 <style>
-
 </style>
---
+++ frontend-chat/src/components/RoomsList.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  interface Room {
+    id: string;
+    name: String;
+  }
+  const rooms: Array<Room> = [
+    {
+      id: "1",
+      name: "main",
+    },
+    {
+      id: "2",
+      name: "news",
+    },
+    {
+      id: "3",
+      name: "random",
+    },
+    {
+      id: "4",
+      name: "ui-ux-design",
+    },
+    {
+      id: "5",
+      name: "code-review",
+    },
+    {
+      id: "6",
+      name: "shared-learn",
+    },
+  ];
+  let activeRoom = 0;
+
+  const selectRoom = (id: number) => {
+    activeRoom = id;
+  };
+</script>
+
+<ul>
+  {#each rooms as room, i}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <li
+      class={`${activeRoom === i? "font-bold bg-yellow-500":""} list-item cursor-pointer uppercase p-2 border-spacing-1 border-cyan-700 border-2 rounded-2xl my-2 hover:bg-gray-300`}
+      on:click={()=>selectRoom(i)}
+    >
+      {room.name}
+    </li>
+  {/each}
+</ul>
+
+<style>
+  /* your styles go here */
+</style>

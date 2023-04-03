e11d1f2d244e07d42b4c03bc043e7657e696a40f
mmaleszewski <mmaleszewski@trans.eu>
2023-04-03 10:10:29 +0200

    chore: add test avatars

---
+++ frontend-chat/src/components/Avatar.svelte
@@ -0,0 +1,16 @@
+<script>
+  // your script goes here
+</script>
+
+<div class="flex items-center">
+  <!-- svelte-ignore a11y-img-redundant-alt -->
+  <div class="h-14 w-14 ">
+    <img class="" src="avatar_1.png" alt="background image" />
+  </div>
+  <div class="font-bold px-2">micJakis</div>
+</div>
+
+<!-- markup (zero or more items) goes here -->
+<style>
+  /* your styles go here */
+</style>
